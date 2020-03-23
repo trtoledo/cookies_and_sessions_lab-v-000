eae4b3a9d28b0e40744671e89935a26d574c8507
Tomas Toledo <trtoledo1977@gmail.com>
2020-03-23 08:10:05 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products, only: [:index, :new]
+
+  root to: 'products#index'
+
+
 end
