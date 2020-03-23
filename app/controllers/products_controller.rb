d0523fdd9d0494f59fca14cee0249cad90b12bc1
Tomas Toledo <trtoledo1977@gmail.com>
2020-03-23 08:23:48 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,15 @@
 class ProductsController < ApplicationController
 
+  def index
+
+  end
+
   def add
     cart << params[:product]
 		redirect_to root_path
   end
 
-  def index
 
-  end
 
 
 end
